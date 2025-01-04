f4a87fbfb66b5abd8feba9f720025d88e9b17bcb
Anjali Sharma <anjali.sharma060202@gmail.com>
2025-01-04 16:45:22 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ const connectDB = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI environment variable is not defined');
   }
-
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
